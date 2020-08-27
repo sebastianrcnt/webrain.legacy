f9d8f93a8a906d856fe9d6143488f9c846fcf341
Sebastian Jeong <sebastianrcnt@gmail.com>
2020-08-28 00:38:30 +0900

    api 로그인 구현

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,41 @@
-const express = require('express')
+const express = require("express");
 const ApiRouter = express.Router();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const jwt = require("jsonwebtoken");
 
-ApiRouter.use('/api/:name', (req, res) => {
-  const name = req.params.name;
-  res.render(`admin/${name}`)
+// ApiRouter.use('/api/:name', (req, res) => {
+//   const name = req.params.name;
+//   res.render(`admin/${name}`)
+// });
+
+ApiRouter.get("/login", async (req, res) => {
+  const { email, password } = req.query;
+
+  if (email && password) {
+    prisma.user.findOne({ where: { email } }).then((found) => {
+      if (found) {
+        if (found.password === password) {
+          const token = jwt.sign(
+            {
+              email,
+            },
+            "secret",
+            { expiresIn: "1d" }
+          );
+
+          req.cookie("webrain-token", token);
+          res.status(200).json(token);
+        } else {
+          res.status(401).send('wrong password')
+        }
+      } else {
+        res.status(404).send("No user find with the email")
+      }
+    });
+  } else {
+    res.status(400).send("Email and Password is not provided");
+  }
 });
 
 module.exports = ApiRouter;
