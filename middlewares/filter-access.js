3a55af869923542dfd5d1153eae1b95be2bc3c31
Sebastian Jeong <sebastianrcnt@gmail.com>
2020-08-28 11:31:55 +0900

    엑세스 제한 및 메세징 템플릿 생성

--- middlewares/filter-access.js
+++ middlewares/filter-access.js
@@ -2,23 +2,31 @@ const PublicRestrictor = () => (req, res, next) => {
   if (req.user) {
     next();
   } else {
-    res.render("utils/message", { message: "허가되지 않은 접근입니다." });
+    res.render("utils/message-with-link", {
+      message: "허가되지 않은 접근입니다.",
+      link: "/admin/login",
+      linkname: "로그인 페이지로 돌아가기",
+    });
   }
 };
 
 // 100 and over => researcher
 // 200 and over => admin
 
-const LevelRestrictor = level => (req, res, next) => {
+const LevelRestrictor = (level) => (req, res, next) => {
   if (req.user && req.user.level >= level) {
-    next()
+    next();
   } else {
-    res.render("utils/message", { message: "허가되지 않은 접근입니다." });
+    res.render("utils/message-with-link", {
+      message: "허가되지 않은 접근입니다.",
+      link: "/admin/login",
+      linkname: "로그인 페이지로 돌아가기",
+    });
   }
-}
+};
 
 module.exports = {
   LevelRestrictor,
-  PublicRestrictor
-}
+  PublicRestrictor,
+};
 // usage app.use(LevelResctrictor(100));
