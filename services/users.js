eb919045c693d1a95e732cca34eb67222b54f836
Sebastian Jeong <sebastianrcnt@gmail.com>
2020-08-29 22:51:19 +0900

    services 업데이트

--- services/users.js
+++ services/users.js
@@ -2,7 +2,7 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const jwt = require("jsonwebtoken");
 
-exports.verifyUser = (email, password) => {
+const verifyUser = (email, password) => {
   const checkExists = (user) => {
     if (user) {
       return Promise.resolve(user);
@@ -38,3 +38,50 @@ exports.verifyUser = (email, password) => {
     .then((user) => checkPassword(user, password))
     .then(signToken);
 };
+
+const createUser = async (user) => {
+  const checkIfConfirmationPasswordMatch = (user) => {
+    if (user.password !== user.password2) {
+      throw {
+        intended: true,
+        message: "확인 비밀번호가 일치하지 않습니다.",
+      };
+    } else {
+      return Promise.resolve(user);
+    }
+  };
+
+  const checkIfEmailExists = async (user) => {
+    const found = await prisma.user.findOne({ where: { email: user.email } });
+    if (found) {
+      throw {
+        intended: true,
+        message: "이미 사용된 이메일입니다",
+      };
+    } else {
+      return Promise.resolve(user);
+    }
+  };
+
+  const writeUserToDatabase = async ({ email, name, phone, password }) => {
+    await prisma.user.create({
+      data: {
+        email,
+        name,
+        phone,
+        password,
+      },
+    });
+  };
+
+  return checkIfConfirmationPasswordMatch(user)
+    .then(checkIfEmailExists)
+    .then(writeUserToDatabase);
+};
+
+const UsersService = {
+  verifyUser,
+  createUser,
+};
+
+module.exports = UsersService;
