34cadb38a86176897fd0cbcf2803a4e11fc3c1fd
Sebastian Jeong <sebastianrcnt@gmail.com>
2020-08-29 22:51:13 +0900

    providers 업데이트

--- providers/index.js
+++ providers/index.js
@@ -1,28 +1,54 @@
 const { PrismaClient } = require("@prisma/client");
+const respondWithError = require("../middlewares/error");
 const prisma = new PrismaClient();
 
 exports.usersContextProvider = () => async (req, res, next) => {
-  const users = await prisma.user.findMany({ where: {} });
-  req.context.users = users;
-  next();
+  prisma.user
+    .findMany({ where: {} })
+    .then((users) => {
+      req.context.users = users;
+      next();
+    })
+    .catch(respondWithError);
+};
+
+exports.userContextProviderByEmail = () => async (req, res, next) => {
+  prisma.user.findOne({where: {email: req.params.email}}).then(user => {
+    if (user) {
+      req.context.user = user;
+      next();
+    } else {
+      res.render('utils/message', {
+        message: "존재하지 않거나 삭제된 유저입니다"
+      })
+    }
+  }).catch(respondWithError);
 };
 
 exports.experimentsContextProvider = () => async (req, res, next) => {
-  const experiments = await prisma.experiment.findMany({ where: {} });
-  req.context.experiments = experiments;
-  next();
+  prisma.experiment
+    .findMany({ where: {} })
+    .then((experiments) => {
+      req.context.experiments = experiments;
+      next();
+    })
+    .catch(respondWithError);
 };
 
-exports.experimentContextProvider = () => async (req, res, next) => {
-  const experiment = await prisma.experiment.findOne({
-    where: { id: req.params.id },
-  });
-  if (experiment) {
-    req.context.experiment = experiment;
-    next();
-  } else {
-    res.render("utils/message", {
-      message: "존재하지 않거나 삭제된 실험입니다.",
-    });
-  }
+exports.experimentContextProviderById = () => async (req, res, next) => {
+  prisma.experiment
+    .findOne({
+      where: { id: req.params.id },
+    })
+    .then((experiment) => {
+      if (experiment) {
+        req.context.experiment = experiment;
+        next();
+      } else {
+        res.render("utils/message", {
+          message: "존재하지 않거나 삭제된 실험입니다.",
+        });
+      }
+    })
+    .catch(respondWithError);
 };
