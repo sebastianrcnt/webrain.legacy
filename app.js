ca30a4ab13dba3eaa2a188d529ace784b082433e
sebastianrcnt <sebastianrcnt@gmail.com>
2020-08-31 01:28:58 +0900

    파일 다운로드 기능

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Create Context
 app.use((req, res, next) => {
