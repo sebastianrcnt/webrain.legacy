d70f3145fb350c5dbed12c564be3085d69688e79
sebastianrcnt <sebastianrcnt@gmail.com>
2020-08-31 00:59:28 +0900

    ' => "로 변경

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
-var express = require('express');
-const AdminRouter = require('./admin');
-const ApiRouter = require('./api');
-const MainRouter = require('./main');
+var express = require("express");
+const AdminRouter = require("./admin");
+const ApiRouter = require("./api");
+const MainRouter = require("./main");
 var router = express.Router();
 
 /* GET home page. */
-router.use('/main', MainRouter)
-router.use('/admin', AdminRouter)
-router.use('/api', ApiRouter)
+router.use("/main", MainRouter);
+router.use("/admin", AdminRouter);
+router.use("/api", ApiRouter);
 
 module.exports = router;
