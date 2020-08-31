5746c347ba2626bf5d5b417a11fe948ecb9a5739
sebastianrcnt <sebastianrcnt@gmail.com>
2020-09-01 03:22:12 +0900

    debug

--- middlewares/error.js
+++ middlewares/error.js
@@ -2,15 +2,14 @@ const respondWithError = (res) => (error) => {
   if (error.intended) {
     res.status(401).render("utils/message", {
       message: error.message,
-    });
+    })
   } else {
-    console.error(error);
-    res.status(500).render("utils/message-with-link", {
+    console.error(error)
+    res.status(500).render("error", {
       message: "알 수 없는 오류가 발생했습니다.",
-      link: "javascript:history.back()",
-      linkname: "뒤로가기",
-    });
+      error,
+    })
   }
-};
+}
 
-module.exports = respondWithError;
+module.exports = respondWithError
